fd9ba132f8629220eb27987529704954aafe163d
Карманов <@KARMANOV>
2017-06-13 15:47:22 +0300

    mongo driver upgrade

--- AppHarborMongoDBDemo/Controllers/BaseController.cs
+++ AppHarborMongoDBDemo/Controllers/BaseController.cs
@@ -16,11 +16,12 @@ namespace AppHarborMongoDBDemo.Controllers
 
 		public  string GetMongoDbConnectionString()
 		{
-       //     return "mongodb://appharbor_pw3dvl7m:eul4dg6sml88gluo42ur4fg2l6@ds159591.mlab.com:59591/appharbor_pw3dvl7m";
-
+            //     return "mongodb://appharbor_pw3dvl7m:eul4dg6sml88gluo42ur4fg2l6@ds159591.mlab.com:59591/appharbor_pw3dvl7m";
+            //     return "mongodb://whereismyguts:VisualStudio15@ds115352.mlab.com:15352/sorokin_sad"
+            //mongodb://<dbuser>:<dbpassword>@ds115352.mlab.com:15352/sorokin_sad
             return
                 //ConfigurationManager.AppSettings.Get("MONGOHQ_URL") ??
-				ConfigurationManager.AppSettings.Get("CUSTOM_MONGOLAB_URI") ??
+                ConfigurationManager.AppSettings.Get("CUSTOM_MONGOLAB_URI") ??
 				"mongodb://localhost/Things";
 		}
 	}
