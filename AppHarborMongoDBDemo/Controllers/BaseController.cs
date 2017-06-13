949bdc52763bd47c88d6a18d95adcaa5e78e2b0c
Карманов <@KARMANOV>
2017-06-13 12:20:26 +0300

    revert

--- AppHarborMongoDBDemo/Controllers/BaseController.cs
+++ AppHarborMongoDBDemo/Controllers/BaseController.cs
@@ -16,9 +16,7 @@ namespace AppHarborMongoDBDemo.Controllers
 
 		public  string GetMongoDbConnectionString()
 		{
-            return "mongodb://whereismyguts:VisualStudio15@ds115352.mlab.com:15352/sorokin_sad";
             return "mongodb://appharbor_pw3dvl7m:eul4dg6sml88gluo42ur4fg2l6@ds159591.mlab.com:59591/appharbor_pw3dvl7m";
-
 
             return ConfigurationManager.AppSettings.Get("MONGOHQ_URL") ??
 				ConfigurationManager.AppSettings.Get("MONGOLAB_URI") ??
