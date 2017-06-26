3d51b37798dbe8ba11c6df2f9c2f126f1ce885a7
Karmanov [w] <whereismyguts@gmail.com>
2017-06-26 11:15:01 +0300

    separate js, meta props, title

---
+++ AppHarborMongoDBDemo/Scripts/manage.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+    $('.image-container').slick();
+
+    var settings = {
+        url: "/Manage/RememberImage",
+        method: "POST",
+        allowedTypes: "jpg,png,gif", //doc,pdf,zip
+        fileName: "myfile",
+        multiple: true,
+        onSuccess: function (files, data, xhr) {
+            $("#status").html("<font color='green'>Upload is success</font>");
+            $("#mulitplefileuploader").append('<div class="thumbnail old-image"> <img src="data:image/png;base64, ' + data + '"></div>')
+
+            //$("#status").html(' <div class="grid"> <img src="data:image/png;base64, ' + data + '"></div>');
+        },
+        onError: function (files, status, errMsg) {
+            $("#status").html("<font color='red'>Upload is Failed</font>");
+        }
+    }
+
+    $("#mulitplefileuploader").uploadFile(settings);
+});
+
+function removeImage(id, elem) {
+    $.ajax({
+        type: "POST",
+        url: "Manage/RemoveImage",
+        data: { id: id },
+        success: function (res) {
+            if (res === "removed") {
+                elem.remove();
+            }
+        },
+        datatype: "json",
+        traditional: true
+    });
+}
---
+++ AppHarborMongoDBDemo/Scripts/sad.js
@@ -0,0 +1,79 @@
+﻿$(document).ready(function () {
+    $('.image-container').slick();
+    refreshCart(false, false);
+});
+function countCardItem(id, count) {
+    $.ajax({
+        type: "POST",
+        url: "/Cart/CountCartItem",
+        data: { itemId: id, delta: count },
+        success: function (res) {
+            refreshCart(true, false);
+        },
+        datatype: "json",
+        traditional: true
+    });
+}
+function removeCardItem(id) {
+    $.ajax({
+        type: "POST",
+        url: "/Cart/RemoveCartItem",
+        data: { itemId: id },
+        success: function (res) {
+            refreshCart(false, true);
+        },
+        datatype: "json",
+        traditional: true
+    });
+}
+function refreshCart(shouldShowCart, shouldAnimate) {
+    $.ajax({
+        type: "POST",
+        url: "/Cart/GetCartItems",
+        success: function (res) {
+            if (res) {
+                $('.cart-sidebar').empty();
+                $('.cart-sidebar').append(res);
+                if (shouldShowCart) {
+                    if (shouldAnimate)
+                        showCart();
+                    else
+                        showCartImediately();
+                }
+                else {
+                    if (shouldAnimate)
+                        hideCart();
+                    else
+                        hideCartImediately();
+                }
+            }
+        },
+        datatype: "json",
+        traditional: true
+    });
+}
+function showCartImediately() {
+    $('.cart-sidebar').css("right", "0px");
+    $('#hide-cart').css("visibility", "visible");
+    $('#show-cart').css("visibility", "hidden");
+}
+function hideCartImediately() {
+    $('.cart-sidebar').css("right", "-370px");
+    $('#hide-cart').css("visibility", "hidden");
+    $('#show-cart').css("visibility", "visible");
+}
+function showCart() {
+    $('.cart-sidebar').css("animation", "0.3s rollin");
+    showCartImediately();
+}
+function hideCart() {
+    $('.cart-sidebar').css("animation", "0.3s rollout");
+    hideCartImediately();
+}
+$(window).scroll(function () {
+    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+        $('.footer').css("opacity", 1);
+    } else {
+        $('.footer').css("opacity", 0);
+    }
+});
